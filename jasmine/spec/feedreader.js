34ddcb7d6cb8e6bd534c0c611909ace66c1a0a70
JudithDea <33676310+JudithDea@users.noreply.github.com>
2018-08-03 09:44:04 -0400

    Updated according to review

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,9 +1,9 @@
-// I utilized QAShahin's Jasmine tutorials on Youtube (https://www.youtube.com/user/GlassBoxT) to learn the basics of the framework. 
+// I utilized QAShahin's Jasmine tutorials on Youtube (https://www.youtube.com/user/GlassBoxT) to learn the basics of the framework.
 // I then completed the project independently.
 
-// new suite starts here
-$(function() {
 
+$(function() {
+// new suite starts here
     describe('RSS Feeds', function() {
 
         // spec 1
@@ -17,7 +17,7 @@ $(function() {
           for (i=0; i<allFeeds.length; i++){
             expect(allFeeds[i].url).toBeDefined();
             expect(allFeeds[i].url.length).toBeGreaterThan(0);
-          };
+          }
         });
 
         // spec 3
@@ -31,62 +31,58 @@ $(function() {
 
 
 // new suite starts here
-$(function(){
-  describe("The Menu", function(){
+    describe("The Menu", function(){
 
       // spec 4
       it('menu element is hidden by default', function(){
-        expect($('body')[0].className).toBe('menu-hidden'); // when we first look at the className, it is "menu-hidden"
+        expect($('body')[0].className).toContain('menu-hidden'); // when we first look at the className, it contains "menu-hidden"
       });
 
       // spec 5
       it('menu changes from hidden to visible on click, and vice versa', function(){
         $('.menu-icon-link').click(); // simulates a click event on the .menu-icon-link element
-        expect($('body')[0].className).not.toBe('menu-hidden'); // after clicking the className is not "menu-hidden" anymore
+        expect($('body')[0].className).not.toContain('menu-hidden'); // after clicking the className is not "menu-hidden" anymore
         $('.menu-icon-link').click(); // simulates the second click event
-        expect($('body')[0].className).toBe('menu-hidden'); // after clicking again the className is "menu-hdden" again
+        expect($('body')[0].className).toContain('menu-hidden'); // after clicking again the className is "menu-hdden" again
       });
-  });
-});
+    });
+
 
 // new suite starts here
-$(function(){
     describe("Initial Entries", function(){
       beforeEach(function(done){
-        loadFeed(0); // making sure the loadFeed function was invoked before each test runs
-        done();
+        loadFeed(1, done); // making sure the loadFeed function was invoked before each test runs
       });
 
       // spec 6
       it('There is at least one .entry within .feed', function(done){
-        expect($('.feed').length).toBeGreaterThan(0); // if there is something inside feed, its length will be greater than 0
+        expect($('.feed .entry').length).toBeGreaterThan(0); // if there is an .entry class inside feed, its length will be greater than 0
         done();
       });
     });
-});
+
 
 // new suite starts there
-$(function(){
     describe('New Feed Selection', function(){
       let feed1,
           feed2;
       beforeEach(function(done){
-        loadFeed(0); // making sure again the loadFeed function was invoked before each test runs
-        feed1 = $('.header-title'); // grabbing the current feed 'name'
-        console.log(feed1);
+        loadFeed(0, function(){
+          feed1 = $('.header-title').innerHTML; // grabbing the current feed 'name'
+          console.log(feed1);
+        }); // making sure again the loadFeed function was invoked before each test runs
         done();
     });
 
     // spec 7
       it('When a new feed is loaded, the content changes', function(done){
-        loadFeed(1); // invoking loadFeed again, now selecting the 2nd element in there
-        feed2 = $('.header-title'); // now we can set the 2nd feed item to compare to first feed item
-        console.log(feed2);
+        loadFeed(1, function(){
+          feed2 = $('.header-title').innerHTML; // now we can set the 2nd feed item to compare to first feed item
+          console.log(feed2);
+        }); // invoking loadFeed again, now selecting the 2nd element in there
         expect('feed1').not.toBe('feed2'); // feed1 should be different from feed2
         done();
       });
     });
-});
 
-         */
 }());
